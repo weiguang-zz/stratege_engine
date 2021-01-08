e25eb43bba025fac5813451a400882997a83b398
zhang <weiguang.zz@alibaba.com>
2021-01-08 13:13:00 +0800

    add sync_db

---
+++ se/interface/sync_db.py
@@ -0,0 +1,13 @@
+
+from se.infras.models import *
+from cassandra.cqlengine.management import sync_table, sync_type
+from se import config
+
+ks_name = config.get("cassandra", "session_keyspace")
+
+sync_type(ks_name, UserOrderModel)
+sync_type(ks_name, OperationModel)
+sync_type(ks_name, DataRecordModel)
+sync_table(AccountModel)
+sync_table(TimeSeriesModel)
+sync_table(TimeSeriesDataModel)
