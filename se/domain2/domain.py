2ddc02e31682552a67671836835769bf5d65ef91
zhang <weiguang.zz@alibaba.com>
2021-03-03 17:35:32 +0800

    增加发送邮件重试的逻辑

--- se/domain2/domain.py
+++ se/domain2/domain.py
@@ -1,9 +1,11 @@
 import logging
 import threading
+from configparser import ConfigParser
 from email.header import Header
 from email.mime.text import MIMEText
 from smtplib import SMTP_SSL
 from typing import Mapping
+import threading
 
 class BeanContainer(object):
 
@@ -26,27 +28,43 @@ def synchronized(func):
 
     return synced_func
 
-@synchronized
+
 def send_email(title: str, content: str):
     from se import config
     if config.get("email", 'activate') == 'false':
         return
-    try:
-        # 登录
-        smtp = SMTP_SSL(config.get('email', 'host_server'))
-        smtp.set_debuglevel(0)
-        smtp.ehlo(config.get('email', 'host_server'))
-        smtp.login(config.get('email', 'username'), config.get('email', 'password'))
 
-        title = '[{}]{}'.format(config.get("ib_account", "name"), title)
-        sender_email = config.get('email', 'sender_email')
-        receiver = config.get('email', 'receiver')
-        msg = MIMEText(content, "plain", 'utf-8')
-        msg["Subject"] = Header(title, 'utf-8')
-        msg["From"] = sender_email
-        msg["To"] = receiver
-        smtp.sendmail(sender_email, receiver, msg.as_string())
-        smtp.quit()
-    except:
-        import traceback
-        logging.error("{}".format(traceback.format_exc()))
+    def send_with_retry():
+        retry_limit = 10
+        for i in range(retry_limit):
+            try:
+                do_send_email(title, content, config)
+                break
+            except:
+                import traceback
+                logging.warning("发送邮件失败 {}".format(traceback.format_exc()))
+                import time
+                time.sleep(10)
+
+    threading.Thread(name='send_email', target=send_with_retry).start()
+
+
+
+
+@synchronized
+def do_send_email(title: str, content: str, config: ConfigParser):
+    # 登录
+    smtp = SMTP_SSL(config.get('email', 'host_server'))
+    smtp.set_debuglevel(0)
+    smtp.ehlo(config.get('email', 'host_server'))
+    smtp.login(config.get('email', 'username'), config.get('email', 'password'))
+
+    title = '[{}]{}'.format(config.get("ib_account", "name"), title)
+    sender_email = config.get('email', 'sender_email')
+    receiver = config.get('email', 'receiver')
+    msg = MIMEText(content, "plain", 'utf-8')
+    msg["Subject"] = Header(title, 'utf-8')
+    msg["From"] = sender_email
+    msg["To"] = receiver
+    smtp.sendmail(sender_email, receiver, msg.as_string())
+    smtp.quit()
